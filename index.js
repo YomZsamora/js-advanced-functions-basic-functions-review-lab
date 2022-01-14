00452b944f310cc18254b527cb5d5f1b5c962fbb
yomZsamora <samora.y@adzumi.co.ke>
2022-01-14 08:31:29 +0300

    Implemented a function called saturdayFun() that uses the default activity 'roller-skate' when no arguments are passed.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code here
+// Uses the default activity 'roller-skate' when no arguments are passed
+function saturdayFun(defaultActivity = 'roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
