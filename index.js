0355cb8104b181ecb774d08d84da0a19b33bcc65
yomZsamora <samora.y@adzumi.co.ke>
2022-01-14 15:41:46 +0300

    Defined wrapAdjective function according to the specification that when initialized with '||' creates a function that, when called, wraps an adjective in a highlight.

--- index.js
+++ index.js
@@ -9,15 +9,8 @@ let mondayWork = function(defaultActivity = 'go to the office') {
 }
 
 // Defines wrapAdjective function according to the specification that when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
-let wrapAdjective = function() {
+let wrapAdjective = function(visualFlairString = '*') {
     return function(adjective = 'special') {
-        return `You are *${adjective}*!`;
+        return `You are ${visualFlairString}${adjective}${visualFlairString}!`;
     }
 }
-
-// function outer(greeting, msg="It's a fine day to learn") { // 2
-//     let innerFunction =  function(name, lang="Python") { // 3
-//       return `${greeting}, ${name}! ${msg} ${lang}` // 4
-//     }
-//     return innerFunction("student", "JavaScript") // 5
-//   }
