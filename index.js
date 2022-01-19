4207b4d527a57c4d3ec1b929ccf7bb9f1b10c8de
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 16:57:02 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 // Uses the default activity 'roller-skate' when no arguments are passed
 function saturdayFun(defaultActivity = 'roller-skate'){
-    return `This Saturday, I want to ${defaultActivity}!`;
+	return `This Saturday, I want to ${defaultActivity}!`;
 }
 
 // Defined mondayWork function expression that uses the default activity 'go to the office' when no arguments are passed.
 let mondayWork = function(defaultActivity = 'go to the office') {
-    return `This Monday, I will ${defaultActivity}.`
+	return `This Monday, I will ${defaultActivity}.`
 }
 
 // Defines wrapAdjective function according to the specification that when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
 let wrapAdjective = function(visualFlairString = '*') {
-    return function(adjective = 'special') {
-        return `You are ${visualFlairString}${adjective}${visualFlairString}!`;
-    }
+	return function(adjective = 'special') {
+		return `You are ${visualFlairString}${adjective}${visualFlairString}!`;
+	}
 }
 
 // JavaScript Object called Calculator that has functions add, substract, multiply and divide.
 let Calculator = {
-    add: function() { return 1 + 3 },
-    subtract: function() { return 1 - 3 }, 
-    multiply: function() { return 1 * 3 },
-    divide: function() { return 10 / 5 }
+	add : (a ,b) =>  a + b,
+	subtract : (a ,b) =>  a - b,
+	multiply : (a ,b) =>  a * b,
+	divide : (a ,b) =>  a / b,
 }
 
 let actionApplyer = function(startingInteger, arrayOfTransforms) {
-    if(arrayOfTransforms[0]) {
-        let multiply = arrayOfTransforms[0](startingInteger);
-        let addition = arrayOfTransforms[1](multiply);
-        return arrayOfTransforms[2](addition);
-    }
-    else {
-        return startingInteger;
-    }
+	if(arrayOfTransforms[0]) {
+		let multiply = arrayOfTransforms[0](startingInteger);
+		let addition = arrayOfTransforms[1](multiply);
+		return arrayOfTransforms[2](addition);
+	}
+	else {
+		return startingInteger;
+	}
 }
 
